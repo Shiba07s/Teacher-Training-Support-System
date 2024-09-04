69e6c09d38bd6bc58df189edeb94aa6a3832c315
Ashutosh Samantaray <116368054+Shiba07s@users.noreply.github.com>
2024-09-04 18:04:20 +0530

    Update SecondDbConfig.java

--- src/main/java/com/TTSS03/DataSourceConfiguration/SecondDbConfig.java
+++ src/main/java/com/TTSS03/DataSourceConfiguration/SecondDbConfig.java
@@ -30,37 +30,7 @@ public class SecondDbConfig {
 	@Autowired
 	private Environment environment;
 	
-	@Bean
-	@Primary
-	public DriverManagerDataSource dataSource1() {
-		
-		 System.out.println("second db connection");
-		
-		 DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		 
-		 dataSource.setUrl(environment.getProperty("second.datasource.url"));
-		 dataSource.setUsername(environment.getProperty("second.datasource.username"));
-		 dataSource.setPassword(environment.getProperty("second.datasource.password"));
-		 dataSource.setDriverClassName(environment.getProperty("second.datasource.driver-class-name"));		
-		return dataSource;
-				
-	}
-	
-	@Bean(name="secondEntityManagerFactoryBean")
-	@Primary
-	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {	
-		LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
-		bean.setDataSource(dataSource1());	
-		JpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		bean.setJpaVendorAdapter(adapter);
-		Map<String,String> props=new HashMap<>();
-		//props.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-		props.put("hibernate.show_sql", "true");
-        bean.setJpaPropertyMap(props);		
-		bean.setPackagesToScan("com.TTSS03.Login.Entity");
-		return bean;
-		
-	}
+	 
 	
 	@Primary
 	@Bean(name="secondTransactionManager")
